506b3a5c3b29fe9e819dce2e04f70390a9ae89bf
elv-wayne <wayne@eluv.io>
2023-04-14 22:47:04 -0400

    Add proper Test folder

--- EluvioWalletTVOS/EluvioWalletTVOS/Test/TestData.swift
+++ EluvioWalletTVOS/EluvioWalletTVOS/Test/TestData.swift
@@ -11,3 +11,25 @@ var test_NFTs: [NFTModel] = loadJsonFile("nfts.json")
 var test_sale_NFTs: [NFTModel] = loadJsonFile("nfts_marketplace.json")
 var test_marketplaces: [MarketplaceModel] = loadJsonFile("marketplaces.json")
 var test_profile: ProfileModel = loadJsonFile("profile.json")
+
+func CreateTestNFTs(num: Int) -> [NFTModel] {
+    var nfts: [NFTModel] = [];
+    for i in 1...num {
+        var item = test_NFTs[0]
+        item.contract_name = "Test NFT " + String(i)
+        item.meta.displayName = "Test NFT Long Name Test NFT Long Name Test NFT Long Name Test NFT Long Name Test NFT Long Name Test NFT Long Name "
+        item.token_id = i
+        item.token_id_str = String(i)
+        
+        let width = Int.random(in: 200..<1000)
+        let height = Int.random(in: 200..<1000)
+        
+        item.meta.image = "https://picsum.photos/\(width)/\(height)"
+        
+        item.ordinal = i
+        item.id = String(i)
+        
+        nfts.append(item)
+    }
+    return nfts
+}
