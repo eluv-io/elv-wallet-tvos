3c3cd92e8319852b2e3d60dc1ca6e28486584324
elv-wayne <wayne@eluv.io>
2023-09-26 14:20:50 -0400

    Add Wallet Link Demo prototype launch pages

--- WalletLinkTest/WalletLinkTest/ContentView.swift
+++ WalletLinkTest/WalletLinkTest/ContentView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 
+/*
 let urls = [
     "FOX - Sports All Access":"elvwallet://items?contract=0x91eaf3bfa0e26cd94f035950e9b79ef3bfa0b1f8&token=1",
     "FOX - Weather All Access":"elvwallet://items?contract=0xeb65174e4ed37a0b99b2f8d130ef84c7cc740264&token=2",
@@ -17,49 +18,69 @@ let urls = [
     "LOTR: The Shire":"elvwallet://items?contract=0x5bb99da3722c0464c8958ce5c4d92f87a87a99b9&token=4126",
     "LOTR: Epic":"elvwallet://items?contract=0xb97c464a16d7f3c2d64f9009da39cc76178c7fd5&token=438",
 ]
+ */
 
 let appStoreUrl = "https://apps.apple.com/in/app/eluvio-media-wallet/id1591550411"
+
+let urls = [
+    "FOX - Weather All Access":"elvwallet://items?contract=0xeb65174e4ed37a0b99b2f8d130ef84c7cc740264&token=2",
+    "FOX - Sports All Access":"elvwallet://items?contract=0x91eaf3bfa0e26cd94f035950e9b79ef3bfa0b1f8&token=1",
+    "The Flash":"elvwallet://items?contract=0x896409ad1da7f3f48749d15602eabac3578694b4&token=630",
+    "One Love":"elvwallet://items"
+]
 
 struct ContentView: View {
     @Environment(\.openURL) private var openURL
     
     var body: some View {
-        VStack(alignment:.center) {
-            HStack(alignment:.center, spacing:20){
-                Image("e_logo")
-                    .resizable()
-                    .frame(width:120, height:120)
-                Text("Eluvio Wallet DeepLink Tester").font(.title)
+        NavigationView {
+            VStack(alignment:.center) {
+                HStack(alignment:.center, spacing:20){
+                    Image("e_logo")
+                        .resizable()
+                        .frame(width:120, height:120)
+                    Text("Eluvio Wallet Link Demo").font(.title)
+                    
+                }
                 
-            }
-            
-            Divider()
-            
-            VStack(alignment:.center){
-                ForEach(urls.sorted(by: >) , id:\.key) { key,value in
-                    Button {
-                        if let url = URL(string: value) {
-                            openURL(url) { accepted in
-                                print(accepted ? "Success" : "Failure")
-                                if (!accepted){
-                                    openURL(URL(string:appStoreUrl)!) { accepted in
-                                        print(accepted ? "Success" : "Failure")
-                                        if (!accepted) {
-                                            print("Could not open URL ", appStoreUrl)
+                Divider()
+                
+                VStack(alignment:.center){
+                    ForEach(urls.sorted(by: <) , id:\.key) { key,value in
+                        NavigationLink(destination:LaunchPage(image: key, link:value)) {
+                            Text(key)
+                                .frame(width:700)
+                        }
+                    }
+                    /*
+                    ForEach(urls.sorted(by: >) , id:\.key) { key,value in
+                        Button {
+                            if let url = URL(string: value) {
+                                openURL(url) { accepted in
+                                    print(accepted ? "Success" : "Failure")
+                                    if (!accepted){
+                                        openURL(URL(string:appStoreUrl)!) { accepted in
+                                            print(accepted ? "Success" : "Failure")
+                                            if (!accepted) {
+                                                print("Could not open URL ", appStoreUrl)
+                                            }
                                         }
                                     }
                                 }
+                                
                             }
-                            
+                        } label: {
+                            Text("\(key)")
                         }
-                    } label: {
-                        Text("\(key)")
                     }
+                    */
                 }
+                .padding(.top,40)
+                
+                Spacer()
             }
-            Spacer()
+            .padding()
         }
-        .padding()
     }
 }
 
---
+++ WalletLinkTest/WalletLinkTest/LaunchPages.swift
@@ -0,0 +1,49 @@
+//
+//  LaunchPages.swift
+//  WalletLinkTest
+//
+//  Created by Wayne Tran on 2023-09-26.
+//
+
+import SwiftUI
+import Combine
+
+struct NoButtonStyle: ButtonStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        return configuration.label
+    }
+}
+
+
+struct LaunchPage: View {
+    @Environment(\.openURL) private var openURL
+    //Fullscreen image prototyping a launch page
+    var image = ""
+    //deeplink into the wallet
+    var link = ""
+
+    var body: some View {
+        Button {
+            debugPrint("Tap")
+            if let url = URL(string: link) {
+                openURL(url) { accepted in
+                    print(accepted ? "Success" : "Failure")
+                    if (!accepted){
+                        openURL(URL(string:appStoreUrl)!) { accepted in
+                            print(accepted ? "Success" : "Failure")
+                            if (!accepted) {
+                                print("Could not open URL ", appStoreUrl)
+                            }
+                        }
+                    }
+                }
+            }
+        } label: {
+            Image(image)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(maxWidth:.infinity, maxHeight:.infinity, alignment: .leading)
+        }
+        .buttonStyle(NoButtonStyle())
+    }
+}
