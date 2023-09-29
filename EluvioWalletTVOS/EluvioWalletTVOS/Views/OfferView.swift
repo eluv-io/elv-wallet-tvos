5021dae6f01ab50051be2ba8d6e51a995c4be7ff
Todd Hodes <todd.hodes@eluv.io>
2023-09-29 13:48:33 -0700

    rm QR for no codes

--- EluvioWalletTVOS/EluvioWalletTVOS/Views/OfferView.swift
+++ EluvioWalletTVOS/EluvioWalletTVOS/Views/OfferView.swift
@@ -359,10 +359,8 @@ struct OfferResultView: View {
             
             // for no more codes
             if fulfill["err"]["op"].stringValue == "no more redemption codes available" {
-                url = "https://eluv.io/"
                 code = ""
                 description = "No more redemption codes available.  Please contact your merchant."
-                error = false
                 return
             }
             setError(cause: "setFullfillment completed with no code or url" + fulfill.stringValue)
